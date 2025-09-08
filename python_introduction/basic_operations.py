e2d8b6c24768e2859f397be83ede8df92e5973fa
sniipe-er <ss.w.rengard@gmail.com>
2025-09-08 14:10:09 +0100

    Add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+print(f"the sum of {number1} and {number2} is {sum_result}")
+print(f"the difference of {number1} and {number2} is {difference_result}")
+print(f"the product of {number1} and {number2} is {product_result}")
