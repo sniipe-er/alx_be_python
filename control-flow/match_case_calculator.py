f71ff33b561d803034e6eceb6972c6b428e751d5
sniipe-er <ss.w.rengard@gmail.com>
2025-09-16 02:27:29 +0100

    Add match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,5 +1,15 @@
 first_number = int (input("Enter the first number:"))
 second_number = int (input("Enter the second number:"))
 operation = str(input("Choose the operation (+, -, *, /):."))
-if operation == "+" :
-    
+if operation == "+":
+    result = first_number + second_number
+elif operation == "-":
+    result = first_number - second_number
+elif operation == "*":
+    result = first_number * second_number
+else:
+    if first_number < 0  or second_number < 0:
+        print("Cannot divide by zero.")
+    else:
+        result = first_number / second_number
+print("The result is ",result)
