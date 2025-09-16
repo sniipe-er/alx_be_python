bcf802ea7914777c933bc3f1b247c50a6fd3002b
sniipe-er <ss.w.rengard@gmail.com>
2025-09-16 08:06:48 +0100

    update the file weather_advice.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,7 @@
 first_number = int (input("Enter the first number:"))
 second_number = int (input("Enter the second number:"))
 operation = input("Choose the operation (+, -, *, /):.")
+
 match operation:
     case '+':
         result = first_number + second_number
