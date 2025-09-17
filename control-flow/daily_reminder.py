bec1f93d3a632b7f49d67d80bc1c07875e664878
sniipe-er <ss.w.rengard@gmail.com>
2025-09-17 14:54:18 +0100

    .

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
-task = input("Enter your task:")
-priority = input("Priority (high, medium, low):")
-time_bound = input("Is it time-bound? (yes/no):")
+task = input("Enter your task: ")
+priority = input("Priority (high, medium, low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
     case 'high':
