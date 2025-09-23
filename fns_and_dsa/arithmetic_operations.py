f4a0c3e32721baf0686e3292c57793ab147921fd
sniipe-er <ss.w.rengard@gmail.com>
2025-09-23 12:48:37 +0100

    .

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
-def perform_operation (num1, num2, operation):
+def perform_operation(num1, num2, operation):
 
-    match operation :
+    match operation:
         case "add":
             return(num1 + num2)
         case "subtract":
