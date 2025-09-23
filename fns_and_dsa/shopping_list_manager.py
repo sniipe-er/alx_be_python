923da8ac427862c93f204f0c9224b4d9b1daec6f
sniipe-er <ss.w.rengard@gmail.com>
2025-09-23 13:29:13 +0100

    .

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,11 +12,11 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter the item to add:")
+            item = input("Enter the item to add: ")
             shopping_list.append
             pass
         elif choice == '2':
-            item = input("Enter the item to remove:")
+            item = input("Enter the item to remove: ")
             shopping_list.remove
             pass
         elif choice == '3':
