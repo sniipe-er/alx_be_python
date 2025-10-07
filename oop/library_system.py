74bc6039e77283fd341e931a11941d046e306080
sniipe-er <ss.w.rengard@gmail.com>
2025-10-08 00:40:06 +0100

    .

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,14 +3,14 @@ class Book:
         self.title = title
         self.author = author
     def __str__(self):
-        return f"'{self.title}' by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
     def __str__(self):
-        return f"EBook: '{self.title}' by {self.author}, File Size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
